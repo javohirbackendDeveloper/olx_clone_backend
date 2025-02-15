fe6e7a95ee76de25ebde400c4202dff128973cf2
javohirfrontenddevs <javohirabdusharipov676@gmail.com>
2025-02-15 22:25:10 +0500

    added another port

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use("/api/product", productRouter);
 app.use("/api/category", categoryRouter);
 app.use("/api/messages", messageRouter);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   console.log("Server is running on the " + PORT);
