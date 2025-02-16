2727ac5e8c5b3ccda43fa1ec9ff1b6c951bf415b
javohirfrontenddevs <javohirabdusharipov676@gmail.com>
2025-02-16 15:34:49 +0500

    s

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -30,13 +30,13 @@ const setCookies = (res, accessToken, refreshToken) => {
   res.cookie("accessToken", accessToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    // sameSite: "None",
     maxAge: 15 * 60 * 1000,
   });
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    // sameSite: "None",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
